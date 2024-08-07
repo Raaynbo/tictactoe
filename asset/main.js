8a966a605196d4225ef179a60f35b4239cad0ded
Raaynbo <rayan.boudjema24@gmail.com>
2024-08-07 20:11:58 +0200

    made a user factory and a gameboard

--- asset/main.js
+++ asset/main.js
@@ -1,2 +1,30 @@
 console.log("tictactoe");
 
+
+const player = function (name) {
+	const playerName = name;
+	let playerSymbol = "";
+
+	const setSymbol = (newSymbol) => playerSymbol = newSymbol;
+	const getSymbol = () => playerSymbol;
+
+	return {playerName, playerSymbol, setSymbol, getSymbol}
+};
+
+const gameboard = (function () {
+	const board = [["","",""],["","",""],["","",""]]; 
+	const setBoard = (x,y,symbol) => board[x][y] =symbol;
+	return {board, setBoard };
+})();
+
+
+const player1 = player("sil", "x");
+
+console.log(player1.getSymbol());
+console.log(player1.setSymbol("o"));
+console.log(player1.getSymbol());
+
+gameboard.setBoard(0,0,player1.getSymbol());
+
+console.log(gameboard.board);
+
