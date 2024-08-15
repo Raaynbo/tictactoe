179e1035cd93141d325e9d31b9662a0353d5657a
Raaynbo <rayan.boudjema24@gmail.com>
2024-08-15 23:35:17 +0200

    player can change empty case

--- asset/main.js
+++ asset/main.js
@@ -1,30 +1,55 @@
 console.log("tictactoe");
 
 
-const player = function (name) {
+const player = function (name, symbol) {
 	const playerName = name;
-	let playerSymbol = "";
+	let playerSymbol = symbol;
 
 	const setSymbol = (newSymbol) => playerSymbol = newSymbol;
 	const getSymbol = () => playerSymbol;
+	const playTurn = (board, x,y) => {if (board.setBoard(x,y,playerSymbol) !== true){
+			console.log("Cant play here");
+			return false;
+		}
+		console.log(`${playerName} played at pos ${x}, ${y}`);
+	};
 
-	return {playerName, playerSymbol, setSymbol, getSymbol}
+	return {playerName, playerSymbol, setSymbol, getSymbol, playTurn}
 };
 
 const gameboard = (function () {
 	const board = [["","",""],["","",""],["","",""]]; 
-	const setBoard = (x,y,symbol) => board[x][y] =symbol;
-	return {board, setBoard };
+	const setBoard = (x,y,symbol) => {
+			if (board[x][y] !== ""){
+			return false;
+			}
+			board[x][y] = symbol;
+			return true;
+		};
+	const resetBoard = () => board.fill(["","",""]);
+	const winCond = () => {
+		for (let i = 0; i<board.length; i++){
+		console.log(i);	
+		}
+	};
+	return {board, setBoard, resetBoard , winCond};
 })();
 
 
 const player1 = player("sil", "x");
+const player2 = player("sisl", "o");
 
-console.log(player1.getSymbol());
-console.log(player1.setSymbol("o"));
-console.log(player1.getSymbol());
 
-gameboard.setBoard(0,0,player1.getSymbol());
+player1.playTurn(gameboard, 0, 1);
 
-console.log(gameboard.board);
+console.table(gameboard.board);
+
+player2.playTurn(gameboard, 0, 0);
+
+player1.playTurn(gameboard, 0, 0);
+console.table(gameboard.board);
+
+gameboard.winCond();
+
+
 
