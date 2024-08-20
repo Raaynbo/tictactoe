50f6473011023d48d2f879bc8ac7d012eb907ba0
Raaynbo <rayan.boudjema@protonmail.com>
2024-08-20 02:44:33 +0200

    add method to check if a player won

--- asset/main.js
+++ asset/main.js
@@ -11,12 +11,14 @@ const player = function (name, symbol) {
 	const playTurn = (board, x,y) => {
 		if (!board.setBoard(x,y, playerSymbol)){
 			console.log("cant play here");
-			return false;
+			return [false, false];
 		}
-		console.table(board.board);
-		console.log(`${playerName} played at pos ${x}, ${y}`);
+		console.log(`${playerName} played at ${x}, ${y}`);
 		playerMoves.push(`${x},${y}`) ;
-		return true;
+		if (playerMoves.length >= 3){
+			return [true , board.isWinnerMove(playerMoves)];
+		}
+		return [true, false];
 		};
 
 	return {playerName, playerSymbol, playerMoves, setSymbol, getSymbol, playTurn}
@@ -24,6 +26,15 @@ const player = function (name, symbol) {
 
 const gameboard = (function () {
 	const board = [["","",""],["","",""],["","",""]]; 
+	const wincase = [["0,0", "0,1", "0,2"],
+			["0,1", "1,1", "2,1"],
+			["0,2", "1,2", "2,2"],
+			["0,0", "1,0", "2,0"],
+			["1,0", "1,1", "1,2"],
+			["2,0", "2,1", "2,2"],
+			["0,0", "1,1", "2,2"],
+			["0,2", "1,1", "2,0"]];
+
 	const setBoard = (x,y,symbol) => {
 			if (board[y][x] !== ""){
 			return false;
@@ -32,14 +43,50 @@ const gameboard = (function () {
 			return true;
 		};
 	const resetBoard = () => board.fill(["","",""]);
-	const winCond = (symbol) => {
-		for (let i = 0; i<board.length; i++){
-		console.log(i);	
-		}
-	};
 
-	const isEmpty = (row) => row.includes("");  
+	const isWinnerMove = (history) => {
+		// this function shouldnt be run until the moveHistory length is eqaul to 3 or higher 
+		// iterate over moveHistory, filter wincase to have all cases having this specific move, then check if the 2 others wincase moves are part of the history list
+		let playerWinningState = 0;
+		let winCond = wincase;
+		let winningMove = [];
+		loop1:
+		for (let i= 0; i < history.length; i++){
+			winCond = winCond.filter((cases) => {
+				if ( cases.includes(history[i])){
+					return cases;
+				}
+			});
 
+			let pWinCond = winCond;
+			loop2:
+			for (let j= 0; j < history.length; j++){
+				if (history[i] === history[j])
+				continue loop2;
+				pWinCond.forEach((possibleWin) => {
+					if (possibleWin.includes(history[j]) == true)
+					{
+						pWinCond = [];
+						pWinCond.push(possibleWin);
+						playerWinningState++;
+					}
+				});
+				if (playerWinningState === 2){
+					console.log("we have a winner here");
+					return true;
+				}
+				if (j === history[j].length-1){
+					pWinCond = wincase;
+					playerWinningState = 0;
+				}
+				console.log(`playerwinningState = ${playerWinningState}`)
+
+
+			}
+			
+		}	
+		return false;
+	};  
 	const canPlay = () => {
 		let empty = false;
 		board.forEach((row) => {
@@ -48,7 +95,7 @@ const gameboard = (function () {
 		return empty;
 	};
 
-	return {board, setBoard, resetBoard , winCond,isEmpty, canPlay};
+	return {board, setBoard, resetBoard, wincase, isWinnerMove, canPlay};
 })();
 
 const gameMode = (function () {
@@ -64,18 +111,20 @@ const gameMode = (function () {
 		// store user input into a var called pos
 		//place marker on board
 		let isDone = false;
+		let isWinner = false;
 		while (!isDone){
 			const x = window.prompt(`${player.playerName}: x pos`);
 			const y = window.prompt(`${player.playerName}: y pos`);
 			const regexp = /[0-2]/;
 			if (x.match(regexp) !== null && y.match(regexp) !== null ){
-				isDone = player.playTurn(board, x,y);
+				[isDone, isWinner] = player.playTurn(board, x,y);
 			}else{
 				console.log("please play in the bound [0-1-2]");
 			}
 	
 		}
-		console.log(player.playerMoves);
+		if (isWinner === true)
+			return true;
 		//check if wincond
 		//return result
 	};
@@ -85,8 +134,11 @@ const gameMode = (function () {
 		
 		while (board.canPlay() ){
 		
-			console.log(nextPlayer);
-			playersTurn(nextPlayer);
+			if (playersTurn(nextPlayer)){
+			console.table(board.board);
+				break;
+			}
+			console.table(board.board);
 		//define who will play this turn in a var called nextPlayer
 		// check if board still has empty cell to play
 			// if not draw
@@ -98,6 +150,7 @@ const gameMode = (function () {
 				nextPlayer = p1;
 			}
 		}
+		console.log(`${nextPlayer.playerName} won !`);
 	};
 	return {p1, p2, board, setPlayers,playersTurn, startGame};
 })();
